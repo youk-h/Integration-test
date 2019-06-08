018b4f4ed1c083808c74053eb9dafffd6d4aa9d5
hashitani <kazurou557@yahoo.co.jp>
2019-06-08 19:20:33 +0900

    [feat] change directory of router.ts and prepare receiving request

--- rest-api-server/router.ts
+++ rest-api-server/router.ts
@@ -1,16 +1,18 @@
 import * as express from "express";
-import * as restApi from "./rest-api";
+import * as restApi from "./handler/rest-api";
 
 const app = express();
 const port = 3000;
 
 const restApiRouter = express.Router();
 
+app.use(express.json());
+
 app.all("*", restApiRouter);
 
-restApiRouter.get("/cars", restApi.getHandler);
+restApiRouter.get("/cars/:name", restApi.getHandler);
 restApiRouter.post("/cars", restApi.postHandler);
-restApiRouter.put("/cars", restApi.putHandler);
-restApiRouter.delete("/cars", restApi.deleteHandler);
+restApiRouter.put("/cars/:name", restApi.putHandler);
+restApiRouter.delete("/cars/:name", restApi.deleteHandler);
 
 app.listen(port, () => console.log(`listen to ${port}`));
