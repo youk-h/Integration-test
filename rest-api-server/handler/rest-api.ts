5fc79b4c2951dce24048d02633bcb297e23c0548
hashitani <kazurou557@yahoo.co.jp>
2019-06-18 21:22:59 +0900

    [fix] add log and change format of response body to json

--- rest-api-server/handler/rest-api.ts
+++ rest-api-server/handler/rest-api.ts
@@ -5,17 +5,20 @@ import * as models from "./rest-api.i";
 import { CarManager } from "../service/car-crud";
 
 export function getHandler(req: Request, res: Response) {
+  console.log("getHandler", req.params);
   const params: models.GetCarParams = {
     name: req.params.name
   };
 
   new CarManager().getCar$(params).subscribe(
     (result) => res.json(result).end(),
-    (error) => res.status(error.code).send(error.message).end(),
+    (error) => res.status(error.code).json(error.message).end(),
   );
 };
 
 export function postHandler(req: Request, res: Response) {
+  console.log("postHandler", req.body);
+
   const params: models.PostCarParams = {
     name: req.body.name,
     maker: req.body.maker,
@@ -23,13 +26,13 @@ export function postHandler(req: Request, res: Response) {
 
   new CarManager().postCar$(params).subscribe(
     (result) => res.json(result).end(),
-    (error) => res.status(error.code).send(error.message).end(),
+    (error) => res.status(error.code).json(error.message).end(),
   );
 };
 
 export function putHandler(req: Request, res: Response) {
-  console.log(req.params)
-  console.log(req.body)
+  console.log("putHandler");
+
   const params: models.PutCarParams = {
     oldName: req.params.name,
     name: req.body.name,
@@ -38,17 +41,19 @@ export function putHandler(req: Request, res: Response) {
 
   new CarManager().putCar$(params).subscribe(
     (result) => res.json(result).end(),
-    (error) => res.status(error.code).send(error.message).end(),
+    (error) => res.status(error.code).json(error.message).end(),
   );
 };
 
 export function deleteHandler(req: Request, res: Response) {
+  console.log("deleteHandler");
+
   const params: models.DeleteCarParams = {
     name: req.params.name,
   };
 
   new CarManager().deleteCar$(params).subscribe(
     (result) => res.json(result).end(),
-    (error) => res.status(error.code).send(error.message).end(),
+    (error) => res.status(error.code).json(error.message).end(),
   );
 };
